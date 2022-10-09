272d11ca11ef4e859e6e77e759b6a31deaf404e5
ganesh2468 <61650646+ganesh2468@users.noreply.github.com>
2022-10-09 10:22:28 +0530

    assessment Temperature checker

--- src/App.js
+++ src/App.js
@@ -1,42 +1,127 @@
-import "./App.css";
-import Contact from "./components/Contact";
-import Skills from "./components/Skills";
-import Hobbies from "./components/Hobbies";
-import Language from "./components/Language";
-import Experience from "./components/Experience";
-import Summary from "./components/Summary";
-import Education from "./components/Education";
-import Projects from "./components/Projects";
+import React,{useState} from 'react';
+//import sample from './videos/fires.mp4';
 
-
-function App() {
-  return (
+const App = () => {
+const[temp,setTemp]=useState(20);
+  const[tempColour,setTempcolour]=useState("normal");
+  const[typeTemp,setTypetemp]=useState("°C");
+  function converter(){
+	const degree=typeTemp;
+	if(degree==="°C"){
+		const newTemp=Math.round(temp*(9/5)+32);
+		setTypetemp("°F");
+		setTemp(newTemp);
+	}
+	if(degree==="°F"){
+		const newTemp=Math.round((temp-32)*(5/9));
+		setTypetemp("°C");
+		setTemp(newTemp);
+	}
+  }
+  function increaseCounter() {
+	const newTemp=temp+1;
+	
+	if(newTemp>30 && typeTemp==="°C"){
+		setTempcolour('redhot');
+	}
+	if(newTemp>28 && newTemp<=30 && typeTemp==="°C"){
+		setTempcolour('hot');
+	}
+	if(newTemp>15 && newTemp<28 && typeTemp==="°C"){
+		setTempcolour('normal');
+	}
+	if(newTemp>10 && newTemp<15 && typeTemp==="°C"){
+		setTempcolour('cold');
+	}
+	if(newTemp<10 && newTemp>5 && typeTemp==="°C"){
+		setTempcolour('colder');
+	}
+	if(newTemp<5 && typeTemp==="°C"){
+		setTempcolour('coldest');
+	}
+	
+	if(newTemp>86 && typeTemp==="°F"){
+		setTempcolour('redhot');
+	}
+	if(newTemp>82.4 && newTemp<=86 && typeTemp==="°F"){
+		setTempcolour('hot');
+	}
+	if(newTemp>59 && newTemp<82.4 && typeTemp==="°F"){
+		setTempcolour('normal');
+	}
+	if(newTemp>50 && newTemp<59 && typeTemp==="°F"){
+		setTempcolour('cold');
+	}
+	if(newTemp<50 && newTemp>41 && typeTemp==="°F"){
+		setTempcolour('colder');
+	}
+	if(newTemp<41 && typeTemp==="°F"){
+		setTempcolour('coldest');
+	}
+	setTemp(newTemp);
+  }
+  function decreaseCounter() {
+	const newTemp=temp-1;
+	if(newTemp>30 && typeTemp==="°C"){
+		setTempcolour('redhot');
+	}
+	if(newTemp>28 && newTemp<=30 && typeTemp==="°C"){
+		setTempcolour('hot');
+	}
+	if(newTemp>15 && newTemp<28 && typeTemp==="°C"){
+		setTempcolour('normal');
+	}
+	if(newTemp>10 && newTemp<15 && typeTemp==="°C"){
+		setTempcolour('cold');
+	}
+	if(newTemp<10 && newTemp>5 && typeTemp==="°C"){
+		setTempcolour('colder');
+	}
+	if(newTemp<5 && typeTemp==="°C"){
+		setTempcolour('coldest');
+	}
+	
+	if(newTemp>86 && typeTemp==="°F"){
+		setTempcolour('redhot');
+	}
+	if(newTemp>82.4 && newTemp<=86 && typeTemp==="°F"){
+		setTempcolour('hot');
+	}
+	if(newTemp>59 && newTemp<82.4 && typeTemp==="°F"){
+		setTempcolour('normal');
+	}
+	if(newTemp>50 && newTemp<59 && typeTemp==="°F"){
+		setTempcolour('cold');
+	}
+	if(newTemp<50 && newTemp>41 && typeTemp==="°F"){
+		setTempcolour('colder');
+	}
+	if(newTemp<41 && typeTemp==="°F"){
+		setTempcolour('coldest');
+	}
+	setTemp(newTemp);
+ }
+	return (
     <>
-      <div class="full">
-        <div class="left">
-          <div class="image">
-          <img src={require('./logo.png')} alt="logo" />
-        </div>
-          <Contact />
-          <Skills  />
-          <Language />
-          <Hobbies />
-        </div>
-        <div class="right">
-          <div class="name">
-            <h1>My Resume</h1>
-          </div>
-          <div class="title">
-            <p>Software Developer</p>
-          </div>
-          <Summary />
-          <Experience />
-          <Education />
-          <Projects />
-        </div>
-      </div>
-    </>
-  );
-}
+		<div className='app-container'>
+			<div className='temperature-display-container'>
+				<div className={`temperature-display ${tempColour}`}>
+          {temp}{typeTemp}</div>
+			</div>
+			<div className='button-container'>
+				<button onClick={increaseCounter}>+</button>
+				<button onClick={decreaseCounter}>-</button>
+				<button onClick={converter}>{typeTemp}</button>
+				{/* <video className='videoTag' autoPlay loop muted>
+					<source src={sample} type='video/mp4' />
+				</video> */}
+			</div>
+		</div> 
+    </> 
+	);
+};
 
 export default App;
+
+//28-30
+//below 10-15 below 5
